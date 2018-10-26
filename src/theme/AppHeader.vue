9e8dbc164969d73a0a72ddbcf37b93afb380f578
D3 <D3@MacBook-Pro-D3.local>
2018-10-26 05:27:07 +0300

    add routes

--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
-    <nav class="nav has-shadow">
-      <div class="container">
-        <a href="/">HEADER_LOGO</a>
-      </div>
-    </nav>
-  </div>
+  <nav class="nav has-shadow">
+    <div class="container">
+      <router-link to="/category/front-end" exact>LOGO</router-link>
+      <router-link :to="{ name: 'category', params: {id: 'front-end' } }" class="nav-item is-tab">Front-end</router-link>
+      <router-link :to="{ name: 'category', params: {id: 'mobile' } }" class="nav-item is-tab">Mobile</router-link>
+      <router-link to="/login" class="nav-item is-tab">Login</router-link>
+    </div>
+  </nav>
 </template>
 
