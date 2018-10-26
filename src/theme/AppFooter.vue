c84fddfb862469eff02bdb423a8a8a5ba0250402
D3 <D3@MacBook-Pro-D3.local>
2018-10-26 03:19:27 +0300

    add components

---
+++ src/theme/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <footer class="footer">
+      <div class="container">
+        <div class="content has-text-centered">
+          Hey there
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
---
+++ src/theme/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <nav class="nav has-shadow">
+      <div class="container">
+        <a href="/">HEADER_LOGO</a>
+      </div>
+    </nav>
+  </div>
+</template>
+
